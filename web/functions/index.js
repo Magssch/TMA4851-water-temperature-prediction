0f1e6f2fcfe6d36d629528597bca290c97c6cdc0
kajasl <kajasofiel@gmail.com>
2021-03-03 16:45:27 +0100

    fetch data from met

--- web/functions/index.js
+++ web/functions/index.js
@@ -1,9 +1,19 @@
 const functions = require('firebase-functions');
+const cors = require("cors")({ origin: true });
+const axios = require("axios");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.getWeatherForecast = functions.https.onRequest( (request, response) => {
+  cors(request, response, () => {
+    return axios.get("https://api.met.no/weatherapi/locationforecast/2.0/compact.json?lat=63.435770&lon=10.390610")
+    .then(r => {
+      response.send(r.data);
+      console.log(r.data)
+      return res.status(200).json({
+        message: response.data.ip
+      })
+    })
+    .catch( e => {
+      response.sendStatus(e);
+    });
+  });
+});
