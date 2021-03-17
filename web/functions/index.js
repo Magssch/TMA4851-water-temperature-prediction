bfd6fab3cd5db4b7c11d9ecf07d36d06a2a108e2
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2021-03-17 14:49:21 +0100

    Fix prediction function

--- web/functions/index.js
+++ web/functions/index.js
@@ -2,45 +2,14 @@ const functions = require("firebase-functions");
 const cors = require("cors")({ origin: true });
 const axios = require("axios");
 const tf = require("@tensorflow/tfjs-node");
-/*
-var admin = require("firebase-admin");
-
-var serviceAccount = require("./tma4851-bathing-temp-pred-firebase-adminsdk-ebzkm-e941943124.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  storageBucket: 'tma4851-bathing-temp-pred.appspot.com'
-});
-
-var bucket =admin.storage().bucket();
-*/
-/*exports.getWeatherForecast = functions.https.onRequest((request, response) => {
-  cors(request, response, () => {
-    return axios
-      .get(
-        "https://api.met.no/weatherapi/locationforecast/2.0/compact.json?lat=63.435770&lon=10.390610"
-      )
-      .then((r) => {
-        response.send(r.data);
-        console.log(r.data);
-        return res.status(200).json({
-          message: response.data.ip,
-        });
-      })
-      .catch((e) => {
-        response.sendStatus(e);
-      });
-  });
-});*/
 
 let objectDetectionModel;
-async function loadModel() {
+async function loadModel(input) {
   // Warm up the model
   if (!objectDetectionModel) {
     // Load the TensorFlow SavedModel through tfjs-node API. You can find more
     // details in the API documentation:
     // https://js.tensorflow.org/api_node/1.3.1/#node.loadSavedModel
-    //file = bucket.ref("saved_model-3.pb");
     objectDetectionModel = await tf.node.loadSavedModel(
       "model",
       ["serve"],
@@ -49,13 +18,21 @@ async function loadModel() {
   }
   var res = null;
   try {
-    var input_data = Array([
-      [11, -0.6, 0.13, -0.6, 1.7],
-      [12, 0.4, 0.12, -1.9, -1.9],
-    ]);
-    var list = objectDetectionModel.predict(input_data);
-    var predictedList = list.toString().split(",");
-    res = predictedList;
+    /*var input_data = tf.tensor([
+      [
+        [11, -0.6, 0.13, -0.6, 1.7],
+        [12, 0.4, 0.12, -1.9, -1.9],
+      ],
+    ]);*/
+    var input_data = tf.tensor(input);
+    console.log(input_data.shape);
+    var predictions = objectDetectionModel.predict(input_data);
+    res = predictions.array().then((array) =>
+      JSON.stringify({
+        data: array,
+      })
+    );
+    console.log(res);
   } catch (err) {
     console.log(err);
   }
@@ -64,8 +41,10 @@ async function loadModel() {
 
 exports.getPred = functions.https.onRequest((request, response) => {
   cors(request, response, () => {
-    return loadModel()
-      .then((r) => response.send(r.data))
-      .catch((e) => {response.sendStatus(e)});
+    return loadModel(JSON.parse(request).data)
+      .then((r) => response.send(r))
+      .catch((e) => {
+        response.sendStatus(e);
+      });
   });
 });
