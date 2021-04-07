cb2333415064059b7af5d639fda92e637f62a3a7
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2021-04-07 23:48:45 +0200

    Fix function timeout

--- web/functions/index.js
+++ web/functions/index.js
@@ -53,63 +53,70 @@ function min_max_inverse_transform(scaled_value, min, max) {
   return scaled_value * (max - min) + min;
 }
 
-exports.getPred = functions.https.onRequest(async (request, response) => {
-  console.log("Fetching data");
-  const look_back = 20;
+const runtimeOpts = {
+  timeoutSeconds: 25,
+  memory: "1GB",
+};
 
-  let wind_forecast = get_data.forecast(63.446521, 10.336418);
+exports.getPred = functions
+  .runWith(runtimeOpts)
+  .https.onRequest(async (request, response) => {
+    console.log("Fetching data");
+    const look_back = 20;
 
-  let temp_hum_forecast = get_data.forecast(63.442876, 10.442747);
+    let wind_forecast = get_data.forecast(63.446521, 10.336418);
 
-  let tide = get_data.tidevann(look_back);
+    let temp_hum_forecast = get_data.forecast(63.442876, 10.442747);
 
-  let wind_historic_data = get_data.historic(
-    "SN68010",
-    "wind_speed,wind_from_direction",
-    look_back
-  );
+    let tide = get_data.tidevann(look_back);
 
-  let temp_hum_historic_data = get_data.historic(
-    "SN68050",
-    "air_temperature,relative_humidity",
-    look_back
-  );
+    let wind_historic_data = get_data.historic(
+      "SN68010",
+      "wind_speed,wind_from_direction",
+      look_back
+    );
 
-  let keys = [
-    "air_temperature",
-    "hour",
-    "relative_humidity",
-    "windx",
-    "windy",
-    "month",
-  ];
+    let temp_hum_historic_data = get_data.historic(
+      "SN68050",
+      "air_temperature,relative_humidity",
+      look_back
+    );
 
-  let values = await parse_data(
-    await wind_forecast,
-    await temp_hum_forecast,
-    await tide,
-    await wind_historic_data,
-    await temp_hum_historic_data,
-    keys
-  );
+    let keys = [
+      "air_temperature",
+      "hour",
+      "relative_humidity",
+      "windx",
+      "windy",
+      "month",
+    ];
 
-  let weather_data = values[0];
-  let dates = values[1];
+    let values = await parse_data(
+      await wind_forecast,
+      await temp_hum_forecast,
+      await tide,
+      await wind_historic_data,
+      await temp_hum_historic_data,
+      keys
+    );
 
-  let input = [];
-  let outputted_dates = [];
-  let outputted_air_temps = [];
+    let weather_data = values[0];
+    let dates = values[1];
 
-  format_input(
-    weather_data,
-    dates,
-    input,
-    outputted_dates,
-    outputted_air_temps,
-    look_back
-  );
+    let input = [];
+    let outputted_dates = [];
+    let outputted_air_temps = [];
 
-  /*
+    format_input(
+      weather_data,
+      dates,
+      input,
+      outputted_dates,
+      outputted_air_temps,
+      look_back
+    );
+
+    /*
 [ 3.60000000e+00 -6.00000000e+00  2.90000000e+01  0.00000000e+00
 1.00000000e-01  1.00000000e+00  5.20000000e+00  1.29000000e+01
 1.56036806e+09 -9.99999736e-01 -9.99999877e-01  6.00000000e-09
@@ -120,21 +127,21 @@ exports.getPred = functions.https.onRequest(async (request, response) => {
 1.60677816e+09 9.99999989e-01 1.31896163e-01 1.10000000e-08
 3.47686270e+00 3.20000000e+00]
 */
-  // water_temperature	air_temperature	relative_humidity	rainfall	wind_speed	wind_direction	tide_obs	tide_pred	timestamp	hour	year	month	windx	windy
+    // water_temperature	air_temperature	relative_humidity	rainfall	wind_speed	wind_direction	tide_obs	tide_pred	timestamp	hour	year	month	windx	windy
 
-  load_model(input)
-    .then((r) => {
-      return response.send(
-        JSON.stringify({
-          water: r
-            .slice(look_back, look_back + 73)
-            .map((value) => min_max_inverse_transform(value, 3.6, 2.14e1)),
-          air: outputted_air_temps
-            .slice(0, 73)
-            .map((row) => min_max_inverse_transform(row, -6.0, 3.21e1)),
-          dates: outputted_dates,
-        })
-      );
-    })
-    .catch((e) => response.sendStatus(e));
-});
+    load_model(input)
+      .then((r) => {
+        return response.send(
+          JSON.stringify({
+            water: r
+              .slice(look_back, look_back + 73)
+              .map((value) => min_max_inverse_transform(value, 3.6, 2.14e1)),
+            air: outputted_air_temps
+              .slice(0, 73)
+              .map((row) => min_max_inverse_transform(row, -6.0, 3.21e1)),
+            dates: outputted_dates,
+          })
+        );
+      })
+      .catch((e) => response.sendStatus(e));
+  });
