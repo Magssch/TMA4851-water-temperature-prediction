315caa7ee38d5e99e07605a0289454729f24c958
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2021-04-21 12:23:54 +0200

    Update App.js

--- web/src/App.js
+++ web/src/App.js
@@ -28,27 +28,56 @@ function App() {
   };
   const [loading, setLoading] = useState(false);
   const [predictionsLoaded, setPredictionsLoaded] = useState(false);
+  const [showGraph, setShowGraph] = useState(false);
   const [predictedWaterTemps, setPredictedWaterTemps] = useState([]);
   const [predictedAirTemps, setPredictedAirTemps] = useState([]);
+
+  const getTempMessage = () => {
+    if (!predictionsLoaded) return "";
+    const avgTemp =
+      predictedWaterTemps.reduce((sum, temp) => sum + temp) /
+      predictedWaterTemps.length;
+
+    setTimeout(() => setShowGraph(true), 5000);
+
+    if (avgTemp < 10) {
+      return "Det er ganske kaldt i vannet nå! 🥶";
+    } else if (avgTemp < 17) {
+      return "Helt OK temperatur 👍";
+    } else {
+      return "Veldig varmt, løp ut og bad! 🥵";
+    }
+  };
 
   return (
     <div className="App">
       <div className="predGraph">
         <Fade top appear={true}>
-          <h1>BadHer BETA 💧</h1>
+          <h1>BadHer BETA</h1>
         </Fade>
         {predictionsLoaded ? (
-          <Fade bottom>
-            <Graph air={predictedAirTemps} water={predictedWaterTemps} />
-          </Fade>
+          <>
+            <Fade bottom when={!showGraph} appear={true} opposite={true}>
+              <p>
+                <br />
+                <br style={{ fontSize: "2rem" }}>{getTempMessage()}</br>
+                <br />
+                <br />
+                <b style={{ fontSize: "1rem" }}>Finn ut hvorfor ⬇️</b>
+              </p>
+            </Fade>
+            {showGraph && (
+              <Fade bottom when={showGraph} appear={true}>
+                <Graph air={predictedAirTemps} water={predictedWaterTemps} />
+              </Fade>
+            )}
+          </>
         ) : (
           <>
             <br />
             <br />
             <Fade left when={!loading} appear={true}>
-              <h2>
-                Hvilken badetemperatur er det i Korsvika de neste 72 timene? 🌡️
-              </h2>
+              <h2>Hvordan er badetemperaturen i Korsvika i morgen?</h2>
             </Fade>
             <br />
             <br />
